bff527a4e85417ca425d7cd1ff9c832b898a0a00
Hasan Iqbal <34169605+hasaniqbal777@users.noreply.github.com>
2024-10-22 16:09:40 +0400

    Added explicit versions to requirements.txt

--- scripts/common.sh
+++ scripts/common.sh
@@ -10,11 +10,6 @@ if ! [ -x "$(command -v git)" ]; then
   exit 1
 fi
 
-if ! [ -x "$(command -v bump2version)" ]; then
-  echo 'Error: bump2version is not installed.' >&2
-  exit 1
-fi
-
 ################################################################################
 # Common Functions                                                             #
 ################################################################################
